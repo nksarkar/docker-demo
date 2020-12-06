0d0364b55c407a05240fa1d7c3886684c4f59cd0
Nayan Sarkar <Nayan.Sarkar@serko.com>
2020-12-06 19:50:36 +1300

    Add terraform to provision azure resources. Container is created with image in docker hub

---
+++ DockerApi/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+    version = "~> 2.13.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_rg" {
+  name                  = "docker-demo-rg"
+  location              = "Australia East"
+}
+
+resource "azurerm_container_group" "tf_container_grp" {
+  name                  = "docker-demo-container-grp"
+  location              = azurerm_resource_group.tf_rg.location
+  resource_group_name   = azurerm_resource_group.tf_rg.name
+  ip_address_type       = "public"
+  dns_name_label        = "dockerdemo"
+  os_type               = "Linux"
+  container {
+    name                = "dockerdemoapi"
+    image               = "nks33/dockerapi"
+    cpu                 = "1"
+    memory              = "1"
+    ports{
+        port            = 80
+        protocol        = "TCP" 
+    }
+  }
+}
