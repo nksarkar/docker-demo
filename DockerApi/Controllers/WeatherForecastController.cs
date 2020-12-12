615b7639cd8b498d5c17d1d9b38946828240a611
Nayan Sarkar <Nayan.Sarkar@serko.com>
2020-12-13 02:17:44 +1300

    Update controller to trigger test

--- DockerApi/Controllers/WeatherForecastController.cs
+++ DockerApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DockerApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "SuperCool", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
